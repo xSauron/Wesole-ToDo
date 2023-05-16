cf536a056a4d41703f0a83c9b8d1e615d7681ab9
Adrian Jasek <xSauron@outlook.com>
2023-05-16 21:50:11 +0200

    Backend update

--- server/index.js
+++ server/index.js
@@ -3,3 +3,9 @@ const axios = require('axios');
 
 const app = express();
 app.use(express.json());
+
+app.listen(4000, () => {
+    console.log('Listening on 4005');
+});
+
+/* USE "NPM START" TO START SERVER*/

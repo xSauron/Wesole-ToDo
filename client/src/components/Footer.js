ba822832c606dc4c921f008261061aaa35bb5466
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2023-05-29 19:00:49 +0200

    Footer Rounded

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,19 @@
 import { GoMarkGithub } from 'react-icons/go';
 
 function Footer() {
-    return (
-        <footer className="px-4 p-4 bg-white text-center shadow dark:bg-gray-800">
-            <div className="flex justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                <span>© 2023 Hydem, Sauron, Kontog23</span>
-                <a
-                    className="ml-2 inline-block"
-                    href="https://github.com/xSauron/Wesole-ToDo"
-                >
-                    {' '}
-                    <GoMarkGithub className="inline-block text-xl pb-1" />
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="px-4 p-4 bg-white text-center rounded shadow dark:bg-gray-800">
+      <div className="flex justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span>© 2023 Hydem, Sauron, Kontog23</span>
+        <a
+          className="ml-2 inline-block"
+          href="https://github.com/xSauron/Wesole-ToDo"
+        >
+          {' '}
+          <GoMarkGithub className="inline-block text-xl pb-1" />
+        </a>
+      </div>
+    </footer>
+  );
 }
 export default Footer;

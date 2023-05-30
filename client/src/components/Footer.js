f9a09dbe585d88ec50f6f6238492f2621349511c
Adrian Jasek <xSauron@outlook.com>
2023-05-30 13:30:25 +0200

    Front Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,23 @@
 import { GoMarkGithub } from 'react-icons/go';
 
 function Footer() {
-  return (
-    <footer className="px-4 p-4 bg-white text-center rounded shadow dark:bg-gray-800">
-      <div className="flex justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        <span>© 2023 Hydem, Sauron, Kontog23</span>
-        <a
-          className="ml-2 inline-block"
-          href="https://github.com/xSauron/Wesole-ToDo"
-        >
-          {' '}
-          <GoMarkGithub className="inline-block text-xl pb-1 hover:text-white" />
-        </a>
-      </div>
-    </footer>
-  );
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className="px-4 p-4 bg-white text-center rounded shadow dark:bg-gray-800">
+            <div className="flex justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                <span>© {currentYear} Hydem, Sauron, Kontog23</span>
+            </div>
+            <div>
+                <a
+                    className="ml-2 inline-block"
+                    href="https://github.com/xSauron/Wesole-ToDo"
+                >
+                    {' '}
+                    <GoMarkGithub className="inline-block text-xl pt-1 hover:text-white" />
+                </a>
+            </div>
+        </footer>
+    );
 }
 export default Footer;
